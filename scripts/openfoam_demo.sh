c81474bcd6d2735d18b687a2686ffea5347d6feb
kaneuffe <kaneuffe@microsoft.com>
2022-09-13 18:42:02 +0000

    Added oepnfoam_demo.sh script to be run on the first openfoam pod

---
+++ scripts/openfoam_demo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Running an OpenFoam job"
+
+source /etc/profile
+module load mpi/hpcx
+source /opt/openfoam10/etc/bashrc
+
+CASE_NAME=motorbike_scaled
+CORES=240
+PPN=120
+NODE1=10.244.3.4
+NODE2=10.244.2.4
+
+cp -r $WM_PROJECT_DIR/tutorials/incompressible/simpleFoam/motorBike $CASE_NAME
+cd $CASE_NAME
+
+# increase blockmesh size
+sed -i 's/(20 8 8)/(40 16 16)/g' system/blockMeshDict
+
+# Determine X,Y,Z based on total cores
+if [ "$(($PPN % 4))" == "0" ]; then
+    X=$(($CORES / 4))
+    Y=2
+    Z=2
+elif [ "$(($PPN % 6))" == "0" ]; then
+    X=$(($CORES / 6))
+    Y=3
+    Z=2
+else
+    echo "Incompataible value of PPN: $PPN. Try something that is divisable by 4,6, or 9"
+    exit -1
+fi
+echo "X: $X, Y: $Y, Z: $Z"
+
+# set up decomposition
+sed -i "s/numberOfSubdomains  6;/numberOfSubdomains $CORES;/g" system/decomposeParDict
+sed -i "s/(3 2 1);/(${X} ${Y} ${Z});/g" system/decomposeParDict
+
+# update runParallel to add MPI flagsM_EXT_LIBIN
+sed -i "s/runParallel\( *\([^ ]*\).*\)$/mpirun -np $CORES -host $NODE1:$PPN,$NODE2:$PPN -x LD_LIBRARY_PATH -x UCX_TLS=rc
+ -x PATH $(env |grep FOAM | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') $(env |grep WM | cut -d'=' -f1 | sed 's/^/-x
+/g' | tr '\n' ' ')-x MPI_BUFFER_SIZE \1 -parallel 2\>\&1 |tee log\.\2/g" Allrun
+
+nohup ./Allrun 2> of_error.txt 1> of_output.txt &
