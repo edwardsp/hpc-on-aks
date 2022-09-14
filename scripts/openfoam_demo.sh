4d3b480495025a6fe313842bfa01cd80826f0595
kaneuffe <kaneuffe@microsoft.com>
2022-09-14 10:45:59 +0000

    Updated openfoam_demo.sh

--- scripts/openfoam_demo.sh
+++ scripts/openfoam_demo.sh
@@ -35,9 +35,7 @@ echo "X: $X, Y: $Y, Z: $Z"
 sed -i "s/numberOfSubdomains  6;/numberOfSubdomains $CORES;/g" system/decomposeParDict
 sed -i "s/(3 2 1);/(${X} ${Y} ${Z});/g" system/decomposeParDict
 
-# update runParallel to add MPI flagsM_EXT_LIBIN
-sed -i "s/runParallel\( *\([^ ]*\).*\)$/mpirun -np $CORES -npernode $PPN -hostfile ~/hostfile -x LD_LIBRARY_PATH -x UCX_TLS=rc
- -x PATH $(env |grep FOAM | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') $(env |grep WM | cut -d'=' -f1 | sed 's/^/-x
-/g' | tr '\n' ' ')-x MPI_BUFFER_SIZE \1 -parallel 2\>\&1 |tee log\.\2/g" Allrun
+# update runParallel to add MPI flags
+sed -i "s/runParallel\( *\([^ ]*\).*\)$/mpirun -np $CORES -npernode $PPN -hostfile ~\/hostfile -x LD_LIBRARY_PATH -x UCX_TLS=rc -x PATH $(env |grep FOAM | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') $(env |grep WM | cut -d'=' -f1 | sed 's/^/-x /g' | tr '\n' ' ') -x MPI_BUFFER_SIZE \1 -parallel 2\>\&1 |tee log\.\2/g" Allrun
 
 nohup ./Allrun 2> of_error.txt 1> of_output.txt &
